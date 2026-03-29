7f7a9feaf51ae3008551ce67b204884a44eb77ce
Charles Iliya Krempeaux <iamreiver@gmail.com>
2026-03-29 16:05:04 -0700

    flag

--- flags.go
+++ flags.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"os"
 	"strings"
 )
@@ -17,4 +18,28 @@ func init() {
 	flag.BoolVar(&flagStdout, "stdout", false, "write to stdout instead of a file")
 
 	flag.Parse()
+}
+
+func outputDirectory() string {
+	args := flag.Args()
+	switch len(args) {
+	case 0:
+		// use current directory
+	case 1:
+		info, err := os.Stat(args[0])
+		if nil != err {
+			fmt.Fprintf(os.Stderr, "ERROR: could not stat %q: %s\n", args[0], err)
+			os.Exit(1)
+		}
+		if !info.IsDir() {
+			fmt.Fprintf(os.Stderr, "ERROR: %q is not a directory\n", args[0])
+			os.Exit(1)
+		}
+		return args[0]
+	default:
+		fmt.Fprintf(os.Stderr, "ERROR: expected at most one directory argument, but got %d\n", len(args))
+		os.Exit(1)
+	}
+
+	return ""
 }
