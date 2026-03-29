cb8aeefbb6232be4b4e2081e6ad34de10d6084f7
Charles Iliya Krempeaux <iamreiver@gmail.com>
2026-03-29 16:01:51 -0700

    flag

--- flags.go
+++ flags.go
@@ -2,10 +2,17 @@ package main
 
 import (
 	"flag"
+	"os"
+	"strings"
 )
 
 var flagStdout bool
 
 func init() {
+	// If we are running inside of a Go test, don't do all thius.
+	if nil != flag.Lookup("test.v") || strings.HasSuffix(os.Args[0], ".test") {
+		return
+	}
+
 	flag.BoolVar(&flagStdout, "stdout", false, "write to stdout instead of a file")
 }
